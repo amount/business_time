230e396da5e3f75e70d11008a492d01168c5b2c3
Dan Rench <daniel.rench@avant.com>
2019-06-11 16:06:13 -0500

    Monkeypatch Integer instead of Fixnum

--- lib/extensions/integer.rb
+++ lib/extensions/integer.rb
@@ -1,16 +1,16 @@
-# hook into fixnum so we can say things like:
+# hook into Integer so we can say things like:
 #  5.business_hours.from_now
 #  7.business_days.ago
 #  3.business_days.after(some_date)
 #  4.business_hours.before(some_date_time)
-class Fixnum
+class Integer
   include BusinessTime
-  
+
   def business_hours
     BusinessHours.new(self)
   end
   alias_method :business_hour, :business_hours
-  
+
   def business_days
     BusinessDays.new(self)
   end
